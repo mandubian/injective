6eeb9d819be6e627a546249577b91c419b028b3a
Pascal Voitot <pascal.voitot.dev@gmail.com>
2014-08-13 16:46:05 +0200

    imported HMonads

--- src/main/scala/ShapelessExt.scala
+++ src/main/scala/ShapelessExt.scala
@@ -1,54 +1,350 @@
 
 import shapeless._
 import poly.Case1
-import ops.hlist.{Prepend, Mapper}
-import ops.nat.{Sum, Pred, ToInt}
+import ops.hlist.{Prepend, Mapper, FlatMapper}
+import ops.nat.{Sum, Pred, ToInt, Prod}
 import syntax.sized._
 import scala.collection.generic.{ CanBuildFrom, IsTraversableLike }
 import scala.collection.{ GenTraversable, GenTraversableLike }
 
-object ShapelessExt {
+object ShapelessExt
+  extends HFunctors
+  with HMonoids
+  with HApplicatives {
 
   trait ~~>[F[_, _], R] extends Poly1 {
     def apply[A, B](f : F[A, B]) : R
     implicit def caseUniv[A, B]: Case.Aux[F[A, B], R] = at[F[A, B]](apply(_))
   }
 
+}
+
+object ShapelessExtImplicits
+  extends HFunctorImplicits
+  with HMonoidImplicits
+  with HApplicativeImplicits
 
 
-  // def toHListMapper[F <: Poly, In <: HList](mapper: HMapper[HList, F, In]) = new Mapper[F, In] {
-  //   type Out = mapper.Out
+trait HMonads extends HPoints {
 
-  //   def apply(l: In): Out = mapper(l)
-  // }
+  trait HMonad[HA, F] {
+    type Out
 
-  // implicit def HListHMapper[F <: Poly, In <: HList](implicit mapper: Mapper[F, In]) = new HMapper[HList, F, In] {
-  //   type Out = mapper.Out
-
-  //   def apply(l: In): Out = mapper(l)
-  // }
-
-  trait HFunctor[HA, F <: Poly] {
-    type Real
-
-    trait HMapper[H, P <: Poly, In] extends DepFn1[In] { type Out }
-
-    val hmapper: HMapper[Real, F, HA]
-
-    def map(ha: HA)(f: F): hmapper.Out
+    def bind(fa: HA)(f: F): Out
   }
 
-  implicit def HListFunctor[HA <: HList, F <: Poly](implicit mapper: Mapper[F, HA]) = new HFunctor[HA, F] {
-    type Real = HList
+  object HMonad extends HPoint {
 
-    val hmapper = new HMapper[HList, F, HA] {
-      type Out = mapper.Out
+    def apply[HA, HF](implicit hm: HMonad[HA, HF]) = hm
 
-      def apply(l: HA): Out = mapper(l)
+  }
+}
+
+object HMonadImplicits
+  extends HMonadImplicits
+  with HPointerImplicits
+
+trait HMonadImplicits extends HMonads with HApplicatives {
+
+  implicit def HListHMonad[HA <: HList, F <: Poly](
+    implicit flatMapper: FlatMapper[F, HA]
+  ) = new HMonad[HA, F] {
+    type Out = flatMapper.Out
+
+    def bind(l: HA)(f: F) = flatMapper(l)
+  }
+
+  implicit def HNilHMonad[F <: Poly, P[_]](
+    implicit flatMapper: FlatMapper[F, HNil]
+  ) = new HMonad[HNil.type, F] {
+    type Out = flatMapper.Out
+
+    def bind(l: HNil.type)(f: F) = flatMapper(l)
+  }
+
+  implicit def ListHMonad[A, F <: Poly, B, P[_]](
+    implicit c: Case1.Aux[F, A, List[B]]
+  ) = new HMonad[List[A], F] {
+    type Out = List[B]
+
+    def bind(l: List[A])(f: F) = l flatMap { a => c(a) }
+  }
+
+  implicit def OptionHMonad[A, F <: Poly, B, P[_]](
+    implicit c: Case1.Aux[F, A, Option[B]]
+  ) = new HMonad[Option[A], F] {
+    type Out = Option[B]
+
+    def bind(l: Option[A])(f: F) = l flatMap { a => c(a) }
+  }
+
+  implicit def OptionSomeHMonad[A, F <: Poly, B, P[_]](
+    implicit c: Case1.Aux[F, A, Option[B]]
+  ) = new HMonad[Some[A], F] {
+    type Out = Option[B]
+
+    def bind(l: Some[A])(f: F) = l flatMap { a => c(a) }
+  }
+
+  implicit def OptionNoneHMonad[A, F <: Poly, B, P[_]](
+    implicit c: Case1.Aux[F, A, Option[B]]
+  ) = new HMonad[None.type, F] {
+    type Out = None.type
+
+    def bind(l: None.type)(f: F) = None
+  }
+
+
+  implicit def SizedHMonad[L, N <: Nat, F <: Poly, E, U, S1, L1, N1 <: Nat, L2, E1, L3](
+    implicit convL : L => GenTraversableLike[E, L],
+             pp    : HPointer.AuxUO[E, U, L],
+             c     : Case1.Aux[F, E, Sized[L1, N1]],
+             convL1: L1 => GenTraversableLike[E1, L1],
+             cbf2  : CanBuildFrom[L, E1, L1],
+             sum   : Sum[N, N1],
+             prod  : Prod[N, N1]
+  ) = new HMonad[Sized[L, N], F] {
+
+    type Out = Sized[L1, prod.Out]
+
+    def bind(l: Sized[L, N])(f: F) = {
+      val ls = l.unsized flatMap { e => c(e).asInstanceOf[Sized[L1, N1]].unsized }
+      Sized.wrap[L1, prod.Out](ls)
     }
-    def map(ha: HA)(f: F): hmapper.Out = hmapper(ha)
   }
 
+}
+
+trait HApplicatives
+  extends HApplies
+  with HFunctors
+  with HPoints {
+
+  trait HApplicative[HA, HF] extends HApply[HA, HF]
+
+  object HApplicative extends HPoint {
+
+    def apply[HA, HF](implicit hap: HApplicative[HA, HF]) = hap
+
+  }
+
+  trait Rel[HA, Up]
+
+  object Rel {
+
+    implicit def HListRel[HA <: HList] = new Rel[HA, HList] {}
+
+    implicit def ListRel[A, F] = new Rel[List[A], List[_]] {}
+  }
+
+}
+
+object HApplicativeImplicits extends HApplicativeImplicits
+
+trait HApplicativeImplicits
+  extends HApplicatives
+  with HApplyImplicits
+  with HPointerImplicits {
+
+  implicit def happlicative[HA, HF, P[_]](
+    implicit happ: HApply[HA, HF]
+  ) = new HApplicative[HA, HF] {
+    type Out = happ.Out
+
+    def ap(ha: => HA)(fs: => HF) = happ.ap(ha)(fs)
+  }
+
+  implicit def hfunctor[HA, F <: Poly, UP, FO](
+    implicit  rel: Rel[HA, UP],
+              hpointer: HPointer.AuxUO[F, UP, FO],
+              happ: HApplicative[HA, FO]
+  ) = new HFunctor[HA, F] {
+    type Out = happ.Out
+
+    def map(ha: HA)(f: F): Out = happ.ap(ha)(hpointer(f))
+  }
+}
+
+trait HPoints extends HPointers {
+
+  trait HPoint {
+
+    // The Point Poly (representing Monad.return in types)
+    def pointF[HA, A](implicit p: TPointer[HA, A]) = Poly {
+      def apply(a: A) = p(a)
+    }
+
+  }
+}
+
+trait HPointers {
+  trait HPointer[H] extends DepFn1[H] {
+    type Up
+    type Out
+  }
+
+  object HPointer {
+    type AuxU[H, U] = HPointer[H] {
+      type Up = U
+    }
+
+    type AuxUO[H, U, O] = HPointer[H] {
+      type Up = U
+      type Out = O
+    }
+  }
+
+  trait TPointer[HA, A] extends DepFn1[A] { type Out = HA }
+
+  object TPointer {
+    type Aux[HA, A, Out0] = TPointer[HA, A] { type Out = Out0 }
+    def apply[HA, A](implicit tp: TPointer[HA, A]): Aux[HA, A, tp.Out] = tp    
+  }
+}
+
+object HPointerImplicits extends HPointerImplicits
+
+trait HPointerImplicits extends HPointers {
+  implicit def HListHPointer[A] = new HPointer[A] {
+    type Up = HList
+    type Out = A :: HNil
+    def apply(a: A) = a :: HNil
+  }
+
+  implicit def HListTPointer[A] = new TPointer[A :: HNil, A] {
+    def apply(a: A) = a :: HNil
+  }
+
+  implicit def ListHPointer[A] = new HPointer[A] {
+    type Up = List[_]
+    type Out = List[A]
+    def apply(a: A) = List(a)
+  }
+
+  implicit def ListTPointer[A] = new TPointer[List[A], A] {
+    def apply(a: A) = List(a)
+  }
+
+  implicit def OptionTPointer[A] = new TPointer[Option[A], A] {
+    def apply(a: A) = Option(a)
+  }
+
+  implicit def SizedTPointer[R[_], A](implicit tp: TPointer[R[A], A]) = 
+    new TPointer[Sized[R[A], Nat._1], A] {
+      def apply(a: A) = Sized.wrap[R[A], Nat._1](tp(a))
+    }
+
+}
+
+
+
+trait HApplies {
+
+  trait HApplier[HF, In] extends DepFn2[In, HF] { type Out }
+
+  trait HApply[HA, HF]{
+    type Up
+    type Out
+
+    def ap(ha: => HA)(fs: => HF): Out
+  }
+
+  object HApply {
+    type Aux[In, HF, Out0] = HApply[In, HF] { type Out = Out0 }
+    type Up[In, HF, Up0] = HApply[In, HF] { type Up = Up0 }
+  }
+}
+
+object HApplyImplicits extends HApplyImplicits
+
+trait HApplyImplicits extends HApplies {
+
+  // HList Apply
+  implicit def HListHApply[HA <: HList, HT <: HList, F <: Poly, OutA <: HList, OutT <: HList, Out0 <: HList](
+    implicit mapper  : Mapper.Aux[F, HA, OutA],
+             tailAp  : HApply.Aux[HA, HT, OutT],
+             prepend : Prepend.Aux[OutA, OutT, Out0]
+  ) = new HApply[HA, F :: HT] {
+    type Up = HList
+    type Out = Out0
+
+    def ap(ha: => HA)(fs: => F :: HT) = prepend(mapper(ha), tailAp.ap(ha)(fs.tail))
+  }
+
+  implicit def HNilHApply[HA] = new HApply[HA, HNil] {
+    type Up = HList
+    type Out = HNil
+
+    def ap(ha: => HA)(fs: => HNil) = HNil
+  }
+
+  implicit def HNil2HApply[HA] = new HApply[HA, HNil.type] {
+    type Up = HList
+    type Out = HNil.type
+
+    def ap(ha: => HA)(fs: => HNil.type) = HNil
+  }
+
+  // List Apply
+  implicit def ListHApply[A, F <: Poly](implicit c: Case1[F, A]) = new HApply[List[A], List[F]] {
+    type Up = List[_]
+    type Out = List[c.Result]
+
+    def ap(l: => List[A])(fs: => List[F]) = fs flatMap { f => l map { a => c(a) } }
+  }
+}
+
+trait HFunctors {
+  trait HFunctor[HA, F <: Poly] {
+    type Out
+
+    def map(ha: HA)(f: F): Out
+  }
+}
+
+object HFunctorImplicits extends HFunctorImplicits
+
+trait HFunctorImplicits extends HFunctors {
+
+  // WARNING: this one shouldn't be needed but apparently scalac requires it for HNil value...
+  implicit def HNilHFunctor[F <: Poly](implicit mapper: Mapper[F, HNil]) = new HFunctor[HNil.type, F] {
+    type Out = mapper.Out
+
+    def map(l: HNil.type)(f: F) = mapper(l)
+  }
+
+  implicit def HListHFunctor[HA <: HList, F <: Poly](implicit mapper: Mapper[F, HA]) = new HFunctor[HA, F] {
+    type Out = mapper.Out
+
+    def map(l: HA)(f: F) = mapper(l)
+  }
+
+  implicit def ListHFunctor[A, F <: Poly](implicit c: Case1[F, A]) = new HFunctor[List[A], F] {
+    type Out = List[c.Result]
+
+    def map(l: List[A])(f: F) = l map { a => c(a) }
+  }
+
+  implicit def OptionHFunctor[A, F <: Poly](implicit c: Case1[F, A]) = new HFunctor[Option[A], F] {
+    type Out = Option[c.Result]
+
+    def map(l: Option[A])(f: F) = l map { a => c(a) }
+  }
+
+  implicit def SizedHFunctor[L, F <: Poly, E, N <: Nat, E1, L1](
+    implicit convL : L => GenTraversableLike[E, L],
+             c     : Case1.Aux[F, E, E1],
+             cbf   : CanBuildFrom[L, E1, L1]
+  ) = new HFunctor[Sized[L, N], F] {
+    type Out = Sized[L1, N]
+
+    def map(l: Sized[L, N])(f: F) = Sized.wrap[L1, N](l.unsized map { e => c(e) })
+  }
+
+}
+
+
+trait HMonoids {
   // HSemiGroup
   trait HSemiGroup[A, B] {
     type Real
@@ -83,6 +379,11 @@ object ShapelessExt {
     }
   }
 
+}
+
+object HMonoidImplicits extends HMonoidImplicits
+
+trait HMonoidImplicits extends HMonoids {
   // Numeric HMonoid
   implicit def NumericHZero[R : Numeric] = new HZero {
     type Real = R
@@ -253,9 +554,9 @@ object ShapelessExt {
   implicit def SizedHSemiGroup[A, B, E, E0 >: E, AN <: Nat, BN <: Nat, CN <: Nat](
     implicit convA : A => GenTraversableLike[E, A],
              convB : B => GenTraversableLike[E0, B],
-             cbf: CanBuildFrom[A, E0, B],
-             hz:  HSemiGroup[A, B] { type Real = B },
-             sum: Sum.Aux[AN, BN, CN]
+             cbf   : CanBuildFrom[A, E0, B],
+             hz    : HSemiGroup[A, B] { type Real = B },
+             sum   : Sum.Aux[AN, BN, CN]
   ) = new HSemiGroup[Sized[A, AN], Sized[B, BN]] {
 
     type Real = Sized[B, Nat]
@@ -267,7 +568,7 @@ object ShapelessExt {
 
   implicit def SizedZeroHSemiGroup[A, B, E, E0 >: E, AN <: Nat, BN <: Nat, CN <: Nat](
     implicit convA : A => GenTraversableLike[E, A],
-             hz: HZero { type Real = Sized[A, Nat]; type Zero = Sized[B, BN] }
+             hz    : HZero { type Real = Sized[A, Nat]; type Zero = Sized[B, BN] }
   ) = new HSemiGroup[Sized[A, AN], Sized[B, BN]] {
 
     type Real = Sized[A, Nat]
@@ -276,4 +577,50 @@ object ShapelessExt {
     def append(a: Sized[A, AN], b: Sized[B, BN]): Out = a
   }
 
+
 }
+
+
+
+  // implicit def hfunctor[HA, F <: Poly](
+  //   implicit //hp  : Rel[HA, U],
+  //            //up  : UPointer[U],
+  //            hm  : HMonad[HA, F]
+  // ) = new HFunctor[HA, F] {
+  //   type Out = hm.Out
+
+  //   def map(ha: HA)(f: F): Out = hm.bind(ha)(f)
+  // }
+
+  // implicit def happlicative[HA, HF, F <: Poly, U, P <: Poly](
+  //   implicit hp     : Rel[HA, U],
+  //            up     : UPointer[U],
+  //            pm     : toPolyHA.Case.Aux[HA, P],
+  //            hm     : HMonad[HF, P]
+  // ) = new HApplicative[HA, HF] {
+  //   type Up = hm.Up
+  //   type Out = hm.Out
+
+  //   val pointer = hm.pointer
+
+  //   def ap(ha: => HA)(fs: => HF): Out = {
+  //     lazy val lha = ha
+
+  //     hm.bind(fs)(pm(ha))
+  //   }
+  // }
+
+  // object toPolyHA extends Poly1 {
+  //   implicit def caseHA[HA, U](
+  //     implicit hp      : Rel[HA, U],
+  //              upointer: UPointer[U]
+  //   ) = at[HA]{ ha => polyHA(ha) }
+  // }
+
+  // def polyHA[HA, U](ha: HA)(implicit 
+  //   hp      : Rel[HA, U],
+  //   upointer: UPointer[U]
+  // ) = new Poly1 {
+  //   implicit def caseP[P <: Poly](implicit fun: HFunctor[HA, P]) = 
+  //     at[P]{ p => upointer(fun.map(ha)(p)) }
+  // }
--- src/test/scala/ShapelessSpec.scala
+++ src/test/scala/ShapelessSpec.scala
@@ -9,6 +9,8 @@ import scalaz.{Free, Coyoneda}
 
 import scala.concurrent._
 import shapeless.test._
+import scala.collection.{ GenTraversable, GenTraversableLike }
+import scala.collection.generic.CanBuildFrom
 
 class ShapelessSpec extends FlatSpec with Matchers {
   import shapeless._
@@ -17,7 +19,8 @@ class ShapelessSpec extends FlatSpec with Matchers {
   import nat._
   import syntax.sized._
 
-  "ShapelessExt" should "manage HMonoid" in {
+  "ShapelessExt" should "manage hmonoid" in {
+    import HMonoidImplicits._
 
     implicit class Rich[A](val a: A) {
       def +[B](b: B)(implicit hm: HMonoid[A, B]) = hm.append(a, b)
@@ -84,16 +87,132 @@ class ShapelessSpec extends FlatSpec with Matchers {
     s72 should equal (Some(10))
   }
 
-  "ShapelessExt" should "manage HFunctor" in {
+  it should "manage HFunctor" in {
+    import HFunctorImplicits._
 
     object f extends Poly1 {
-      implicit def caseInt     = at[Int](x => "foo:"+x.toString)
-      implicit def caseBoolean = at[Boolean](x => "foo:"+x.toString)
-      implicit def caseString  = at[String](x => "foo:"+x)
+      implicit def caseInt     = at[Int]    (x => "foo_"+x.toString)
+      implicit def caseBoolean = at[Boolean](x => "foo_"+x.toString)
+      implicit def caseString  = at[String] (x => "foo_"+x)
     }
 
-    def map[H, HA <: H, HB <: H](ha: HA)(f: Poly)(implicit hf: HFunctor[HA, f.type]) = hf.map(ha)(f)
-    println("RES:"+map(1 :: "string" :: HNil)(f))
+    object g extends Poly1 {
+      implicit def caseString  = at[String] (x => Option(x))
+    }
+
+    def map[HA](ha: HA)(f: Poly)(implicit hf: HFunctor[HA, f.type]) = hf.map(ha)(f)
+
+    map(1 :: "string" :: HNil)(f) should equal ("foo_1" :: "foo_string" :: HNil)
+    map(HNil)(f) should equal (HNil)
+
+    map(List(1, 2, 3))(f) should equal (List("foo_1", "foo_2", "foo_3"))
+    map(List("alpha", "beta", "gamma"))(f) should equal (List("foo_alpha", "foo_beta", "foo_gamma"))
+    map(List.empty[Int])(f) should equal (List())
+
+    map(map(List(1, 2, 3))(f))(g) should equal (map(List(1, 2, 3))(g compose f))
+
+    map(List(1, 2, 3).sized(3).get)(f).unsized should equal (List("foo_1", "foo_2", "foo_3"))
+    map(List.empty[Int].sized(0).get)(f).unsized should equal (List())
+  }
+
+
+  it should "manage HApplicative" in {
+    import HApplicativeImplicits._
+
+    object f extends Poly1 {
+      implicit def caseInt     = at[Int]    (x => "foo_"+x.toString)
+      implicit def caseBoolean = at[Boolean](x => "foo_"+x.toString)
+      implicit def caseString  = at[String] (x => "foo_"+x)
+    }
+
+    object g extends Poly1 {
+      implicit def caseString  = at[String] (x => Option(x))
+      implicit def caseInt     = at[Int] (x => Option(x))
+    }
+
+
+    def apply[HA, HF](ha: HA)(f: HF)(implicit happ: HApplicative[HA, HF]) = happ.ap(ha)(f)
+
+    apply(1 :: "string" :: HNil)(f :: g :: HNil) should equal ("foo_1" :: "foo_string" :: Some(1) :: Some("string") :: HNil)
+
+    // not really useful as polymorphism in a List[A] isn't possible
+    apply(List(1, 2, 3))(List(f)) should equal (List("foo_1", "foo_2", "foo_3"))
+
+    HApplicative.pointF[Int :: HNil, Int].apply(5) should equal (5 :: HNil)
+
+    HApplicative.pointF[String :: HNil, String].apply("toto") should equal ("toto" :: HNil)
+
+    // Functors from Applicatives
+    implicitly[HFunctor[Int :: String :: HNil, f.type]]
+    implicitly[HFunctor[List[Int], f.type]]
+    implicitly[HFunctor[List[String], f.type]]
+  }
+
+  it should "manage HMonad" in {
+    import HMonadImplicits._
+    import ops.hlist.{Prepend, Mapper, FlatMapper}
+
+    object f extends Poly1 {
+      implicit def caseInt     = at[Int]    (x => x :: x + 1 :: HNil)
+      implicit def caseInt2    = at[Int]    (x => List(x, x + 1))
+      implicit def caseInt3    = at[Int]    (x => List(x, x + 1).sized(2).get)
+      implicit def caseInt4    = at[Int]    (x => Some(x + 1):Option[Int])
+
+      implicit def caseBoolean = at[Boolean](x => !x :: HNil)
+      implicit def caseString  = at[String] (x => x + "tutu" :: HNil)
+      implicit def caseString2 = at[String] (x => List(x, "tutu"))
+    }
+
+    def bind[HA, HF](ha: HA)(f: HF)(implicit happ: HMonad[HA, HF]) = happ.bind(ha)(f)
+
+    bind(1 :: "string" :: true :: HNil)(f) should equal (1 :: 2 :: "stringtutu" :: false :: HNil)
+
+    bind(List(1, 5, 10))(f) should equal (List(1, 2, 5, 6, 10, 11))
+    bind(List("1", "5", "10"))(f) should equal (List("1", "tutu", "5", "tutu", "10", "tutu"))
+
+    bind(Some(5))(f) should equal (Some(6))
+    bind(None)(f) should equal (None)
+
+    val hsz = bind(List(3, 4, 5).sized(3).get)(f)
+    hsz.unsized should equal (List(3, 4, 4, 5, 5, 6))
+    typed[Sized[List[Int], _6]](hsz)
+
+  }
+
+
+  it should "manage HMonad Laws" in {
+    import HMonadImplicits._
+    import ops.hlist.{Prepend, Mapper, FlatMapper}
+
+    object f extends Poly1 {
+      implicit def caseInt     = at[Int]    (x => s"$x" :: s"${x + 1}" :: HNil)
+      implicit def caseString  = at[String] (x => x + "tutu" :: HNil)
+    }
+
+    object g extends Poly1 {
+      implicit def caseString  = at[String] (x => Option(x) :: HNil)
+    }
+
+    def bind[HA, HF](ha: HA)(f: HF)(implicit hm: HMonad[HA, HF]) = hm.bind(ha)(f)
+
+    // Associativity
+    // (m >>= f) >>= g   ≡   m >>= ( \x -> (f x >>= g) )
+    object h extends Poly1 {
+      implicit def caseI[I, O](
+        implicit c: f.Case.Aux[I, O], hm: HMonad[O, g.type]
+      ) = at[I] (i => bind(f(i))(g))
+    }
+
+    bind(bind(1 :: "toto" :: HNil)(f))(g) should equal (bind(1 :: "toto" :: HNil)(h))
+
+    // Neutral Element
+    val hm = HMonad[Int :: HNil, f.type]
+    val pf = HMonad.pointF[Int :: HNil, Int]
+    // (return x) >>= f   ≡   f x
+    hm.bind(pf(1))(f) should equal (f(1))
+
+    // m >>= return   ≡   m
+    HMonad[Int :: HNil, pf.type].bind(1 :: HNil)(pf) should equal (1 :: HNil)
   }
 
 }
